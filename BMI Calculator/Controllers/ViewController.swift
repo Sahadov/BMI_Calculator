a743496c459a4ebc4962da0c03b69ddb446c5d69
Dmitry <mrsahadov@gmail.com>
2024-06-05 14:49:25 -0300

    add logic for the sliders

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        var height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        var weight = String(format: "%.f", sender.value)
+        weightLabel.text = "\(weight)kg"
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
